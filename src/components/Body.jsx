6662c788dbbbf3abd1578af4e35803e9c387aa42
Vy <motchiecvee@Little-Vee.local>
2023-03-08 13:57:45 +0700

    ex-buoi1

---
+++ src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './Banner'
+import Item from './Item'
+
+function Body() {
+  return (
+    <div>
+        <Banner/>
+        <Item/>
+    </div>
+    
+  )
+}
+
+export default Body
---
+++ src/components/Heroic.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Body from './Body'
+import Footer from './Footer'
+import Header from './Header'
+
+function Heroic() {
+  return (
+    <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+    </div>
+
+  )
+}
+
+export default Heroic
